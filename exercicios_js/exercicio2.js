c18f49302c8ceb7e3bfcb30d006622364297c0e5
joaov <jvictorcs35@gmail.com>
2025-12-08 21:47:52 -0300

    dor-e-sofrimento

---
+++ exercicios_js/exercicio2.js
@@ -0,0 +1,24 @@
+//Peça dois números e uma operação matemática (+, -, *, /).
+// Calcule e mostre o resultado da operação escolhida.
+
+//entrada
+const num1 = Number(prompt("Digite o primeiro número"))
+const num2 = Number(prompt("Digite o segundo número"))
+const operação = prompt("Digita ai uma operação: (+, -, *, /) ")
+
+//processamento
+if (operação === "+") {
+    alert("O resultado é " +(num1+num2))
+}
+else if (operação === "-") {
+    alert("O resultado é " +(num1-num2))
+}
+else if (operação === "*") {
+    alert("O resultado é " +(num1*num2))
+}
+else {
+    alert("O resultado é " +(num1/num2))
+}
+//saida
+
+// não tem KKKLKKKLKLLKKLLKLLK
